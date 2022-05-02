2ba5289c0e7f1cd5473f77084aa0e386e1a102d3
Md Anas Sabah <mdanassabah@gmail.com>
2022-05-02 10:37:21 +0530

    Authentication Approved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db, auth } from "./firebase";
+import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { Button } from "@mui/material";
+import { Input } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "md_anas_sabah",
-      caption: "Make Them Stop and Stare😎",
-      imageURL:
-        "https://media-exp1.licdn.com/dms/image/C5603AQGAtM_odmLb7g/profile-displayphoto-shrink_200_200/0/1638384912500?e=1654732800&v=beta&t=TGs1dlGQfsjY2RtyG8fP03YS4zGsiIFEQf_VCPZYGE0",
-    },
-    {
-      username: "iamsrk",
-      caption:
-        "Shah Rukh agar thoda ruk bhi gaya to pathan ko kaise rokoge.. Apps aur Abs sbko bana dalunga..",
-      imageURL: "https://im.rediff.com/movies/2022/mar/28insta1.jpg",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in.
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        //user has logged out.
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //perform some cleanup action.
+      unsubscribe();
+    };
+  }, [user, username]);
+
+  // UseEffect: Runs a piece of code based on specific condition.
+  useEffect(() => {
+    db.collection("post").onSnapshot((snapshot) => {
+      //every time a new post is added, this code fired.
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => alert(err.message));
+
+    setOpenSignIn(false);
+  };
 
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <center>
+              <img
+                src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+                alt=""
+              />
+            </center>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <form>
+              <center className="app_signUp">
+                <Input
+                  placeholder="Username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <Input
+                  placeholder="Email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button type="submit" onClick={handleLogin}>
+                  SignUp
+                </Button>
+              </center>
+            </form>
+          </Typography>
+        </Box>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <center>
+              <img
+                src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+                alt=""
+              />
+            </center>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <form>
+              <center className="app_signUp">
+                <Input
+                  placeholder="Email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button type="submit" onClick={signIn}>
+                  SignIn
+                </Button>
+              </center>
+            </form>
+          </Typography>
+        </Box>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -28,11 +167,21 @@ function App() {
         />
       </div>
 
-      {posts.map((post) => (
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpen(true)}>SignIn</Button>
+          <Button onClick={() => setOpen(true)}>SignUp</Button>
+        </div>
+      )}
+
+      {posts.map(({ id, post }) => (
         <Post
+          key={id}
           username={post.username}
           caption={post.caption}
-          imageURL={post.imageURL}
+          imageURL={post.imageUrl}
         />
       ))}
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
-import firebase from "./firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyC01T81nAIX2rPskHtxBltRKFrGUj6zyD8",
